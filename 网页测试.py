532126809b6818dc8e5e82ea71d9f642e91a3809
lovedavid78 <fenghua@fenghuadeMacBook-Pro.local>
2024-10-05 10:42:50 +0800

    修正

--- 网页测试.py
+++ 网页测试.py
@@ -1,6 +1,6 @@
 import requests
 
-def check_website(url, timeout=5):
+def check_website(url='www.google.com', timeout=5):
     # Check if the URL starts with 'http://' or 'https://'
     if not url.startswith(('http://', 'https://')):
         url = 'http://' + url
